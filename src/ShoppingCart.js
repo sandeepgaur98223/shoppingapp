5b1683837b42f35ce688647603f3f1317003e42c
Sandeep Gaur <sandeepgaur98223@gmail.com>
2023-06-19 21:58:46 +0530

    added edit

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,21 +1,38 @@
 import React, { useState } from 'react';
 import './ShoppingCart.css';
 
-
 const ShoppingCart = () => {
   const [items, setItems] = useState([]);
   const [itemName, setItemName] = useState('');
   const [itemPrice, setItemPrice] = useState('');
   const [showForm, setShowForm] = useState(false);
+  const [editItemId, setEditItemId] = useState(null);
 
   const addItem = () => {
     if (itemName && itemPrice) {
-      const newItem = {
-        id: Math.random().toString(36).substr(2, 9),
-        name: itemName,
-        price: itemPrice
-      };
-      setItems([...items, newItem]);
+      if (editItemId !== null) {
+        // Editing an existing item
+        const updatedItems = items.map(item => {
+          if (item.id === editItemId) {
+            return {
+              ...item,
+              name: itemName,
+              price: itemPrice
+            };
+          }
+          return item;
+        });
+        setItems(updatedItems);
+        setEditItemId(null);
+      } else {
+        const newItem = {
+          id: Math.random().toString(36).substr(2, 9),
+          name: itemName,
+          price: itemPrice
+        };
+        setItems([...items, newItem]);
+      }
+
       setItemName('');
       setItemPrice('');
       setShowForm(false);
@@ -27,6 +44,16 @@ const ShoppingCart = () => {
     setItems(updatedItems);
   };
 
+  const editItem = (id) => {
+    const itemToEdit = items.find(item => item.id === id);
+    if (itemToEdit) {
+      setItemName(itemToEdit.name);
+      setItemPrice(itemToEdit.price);
+      setEditItemId(id);
+      setShowForm(true);
+    }
+  };
+
   return (
     <div className="shopping-cart">
       <h1>Shopping Cart</h1>
@@ -34,13 +61,14 @@ const ShoppingCart = () => {
         {items.map(item => (
           <li key={item.id}>
             {item.name} - ${item.price}
+            <button onClick={() => editItem(item.id)}>Edit</button>
             <button onClick={() => removeItem(item.id)}>Remove</button>
           </li>
         ))}
       </ul>
       {showForm ? (
         <div>
-          <h2>Add Item</h2>
+          <h2>{editItemId !== null ? 'Edit Item' : 'Add Item'}</h2>
           <div>
             <label htmlFor="itemName">Name: </label>
             <input
@@ -59,7 +87,7 @@ const ShoppingCart = () => {
               onChange={(e) => setItemPrice(e.target.value)}
             />
           </div>
-          <button onClick={addItem}>Add to Cart</button>
+          <button onClick={addItem}>{editItemId !== null ? 'Save' : 'Add to Cart'}</button>
         </div>
       ) : (
         <button onClick={() => setShowForm(true)}>Add Item</button>
